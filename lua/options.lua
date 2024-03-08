97cdfc37b466881b229f4f95b1c9380ae48aeb4a
daniele <daniele@localhost-live.home-life.hub>
2024-03-08 16:47:13 +0100

    added some basic config files

---
+++ lua/options.lua
@@ -0,0 +1,27 @@
+-- set leader key (useful for plugins)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- show row numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- do not show current mode in status line
+vim.opt.showmode = false
+
+-- use system clipboard
+vim.opt.clipboard = 'unnamedplus'
+
+-- ignore case in searches
+vim.opt.ignorecase = true
+
+-- keep signcolumn on by default
+vim.opt.signcolumn = 'yes'
+
+-- set where to open split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- highlight cursor current line
+vim.opt.cursorline = false
+
