0abc9522c4774a8e120d039eebe391ddaa18c4cd
Daniele Muffato <danixgithub1@gmail.com>
2024-03-10 18:54:39 +0100

    added treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
     { 
         require('plugins/colorscheme'),
-        require('plugins/statusline')
+        require('plugins/statusline'),
+        require('plugins/treesitter'),
     },
 })
