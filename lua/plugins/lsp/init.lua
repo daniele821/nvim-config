7ed33ccb67571d1a55bcfa0a726637c2d85702c0
Daniele Muffato <danixgithub1@gmail.com>
2024-04-12 14:13:36 +0200

    fixes

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,6 +1,5 @@
 return {
-    require('plugins/lsp/telescope'),
-    require('plugins/lsp/lspconfig'),
-    require('plugins/lsp/autoformat'),
-    require('plugins/lsp/completion'),
+	require("plugins/lsp/telescope"),
+	require("plugins/lsp/lspconfig"),
+	require("plugins/lsp/autoformat"),
 }
