56c8df42ca630f42fbaf91da16e7ee097516907f
Daniele Muffato <danixgithub1@gmail.com>
2024-04-13 18:17:21 +0200

    added linters support and ensure-installed shellcheck

---
+++ lua/plugins/lsp/linter.lua
@@ -0,0 +1,16 @@
+return {
+	"mfussenegger/nvim-lint",
+	config = function()
+		require("lint").linters_by_ft = {
+			sh = { "shellcheck" },
+			bash = { "shellcheck" },
+		}
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+			callback = function()
+				-- try_lint without arguments runs the linters defined in `linters_by_ft`
+				-- for the current filetype
+				require("lint").try_lint()
+			end,
+		})
+	end,
+}
