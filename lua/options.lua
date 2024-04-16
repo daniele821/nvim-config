1f14263202d05fb36ce8967184bc1b7f3c2ce7c6
Daniele Muffato <danixgithub1@gmail.com>
2024-04-16 15:41:12 +0200

    removed others file

--- lua/options.lua
+++ lua/options.lua
@@ -32,3 +32,18 @@ vim.opt.splitbelow = true
 
 -- highlight cursor current line
 vim.opt.cursorline = false
+
+-- highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- disable auto-comment (hacky fix)
+vim.api.nvim_create_autocmd("BufEnter", {
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
