4c49121acf8f85aeeb7c04be072f9609ae431f42
Daniele Muffato <danixgithub1@gmail.com>
2024-04-18 22:06:50 +0200

    changed undotree keymap

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -4,7 +4,7 @@ return {
 		local function toggleUndo()
 			vim.cmd(":UndotreeToggle")
 		end
-		vim.keymap.set("n", "<leader>t", toggleUndo)
+		vim.keymap.set("n", "<A-t>", toggleUndo)
 		vim.g.undotree_WindowLayout = 3
 		vim.g.undotree_SetFocusWhenToggle = 1
 		vim.g.undotree_SplitWidth = 40
