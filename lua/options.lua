86ca70341f2e50e3cfa93674a5ef0478a4298655
Daniele Muffato <danixgithub1@gmail.com>
2024-04-22 14:11:50 +0200

    small hacky fix for sql files

--- lua/options.lua
+++ lua/options.lua
@@ -48,3 +48,11 @@ vim.api.nvim_create_autocmd("BufEnter", {
 	end,
 })
 
+-- when moving with arrows in sql files, it prints some garbage idk why
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = "sql",
+	callback = function()
+		vim.keymap.del("i", "<left>", { buffer = true })
+		vim.keymap.del("i", "<right>", { buffer = true })
+	end,
+})
