cb1b57f56338579b691b49281fbb86bacc4b7dd2
Daniele Muffato <danixgithub1@gmail.com>
2024-04-24 19:28:50 +0200

    separated autocmds into a separate file

---
+++ lua/autocmd.lua
@@ -0,0 +1,23 @@
+-- highlight when yanking text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- disable auto-comment (hacky fix)
+vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
+	callback = function()
+		vim.opt.formatoptions:remove({ "c", "r", "o" })
+	end,
+})
+
+-- when moving with arrows in sql files, it prints some garbage idk why
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = "sql",
+	callback = function()
+		vim.keymap.del("i", "<left>", { buffer = true })
+		vim.keymap.del("i", "<right>", { buffer = true })
+	end,
+})
--- lua/options.lua
+++ lua/options.lua
@@ -32,27 +32,3 @@ vim.opt.splitbelow = true
 
 -- highlight cursor current line
 vim.opt.cursorline = false
-
--- highlight when yanking text
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
-
--- disable auto-comment (hacky fix)
-vim.api.nvim_create_autocmd({ "BufEnter", "FileType" }, {
-	callback = function()
-		vim.opt.formatoptions:remove({ "c", "r", "o" })
-	end,
-})
-
--- when moving with arrows in sql files, it prints some garbage idk why
-vim.api.nvim_create_autocmd("Filetype", {
-	pattern = "sql",
-	callback = function()
-		vim.keymap.del("i", "<left>", { buffer = true })
-		vim.keymap.del("i", "<right>", { buffer = true })
-	end,
-})
