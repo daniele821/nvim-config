6d1f5672a8a310a926ce0173afcb2c173de29b60
Daniele Muffato <danixgithub1@gmail.com>
2024-05-16 18:33:01 +0200

    added mapping to toggle inlay hints, if available (wait for 0.10)

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -44,6 +44,14 @@ return {
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
 				map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+				-- add mapping to toggle inlay hints
+				local client = vim.lsp.get_client_by_id(event.data.client_id)
+				if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+					map("<leader>th", function()
+						vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+					end, "[T]oggle Inlay [H]ints")
+				end
 			end,
 		})
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,8 +2,9 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
 	config = function()
+		---@diagnostic disable-next-line: missing-fields
 		require("nvim-treesitter.configs").setup({
-			-- ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+			ensure_installed = {},
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
