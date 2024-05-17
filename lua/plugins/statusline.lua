d7197ec94748b2863087b3953b3fa78ec22a6dcc
Daniele Muffato <danixgithub1@gmail.com>
2024-05-17 20:22:03 +0200

    changed fidget with lsp-progress

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,4 +1,28 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	opts = {},
+	dependencies = {
+		"linrongbin16/lsp-progress.nvim",
+		opts = {},
+	},
+	config = function()
+		require("lualine").setup({
+			sections = {
+				lualine_c = {
+					"filename",
+					function()
+						-- invoke lsp progress here
+						return require("lsp-progress").progress()
+					end,
+				},
+			},
+		})
+
+		-- listen lsp-progress event and refresh lualine immediatly
+		vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+		vim.api.nvim_create_autocmd("User", {
+			group = "lualine_augroup",
+			pattern = "LspProgressStatusUpdated",
+			callback = require("lualine").refresh,
+		})
+	end,
 }
