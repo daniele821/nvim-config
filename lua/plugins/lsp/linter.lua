164e1dcaff2f4e35b060c8bf6c109fe6bb417e72
Daniele Muffato <danixgithub1@gmail.com>
2024-05-18 11:28:09 +0200

    small optimizations by storing loaded modules in variables whenever possible

--- lua/plugins/lsp/linter.lua
+++ lua/plugins/lsp/linter.lua
@@ -1,13 +1,14 @@
 return {
 	"mfussenegger/nvim-lint",
 	config = function()
-		require("lint").linters_by_ft = {
+		local lint = require("lint")
+		lint.linters_by_ft = {
 			sh = { "shellcheck" },
 			bash = { "shellcheck" },
 		}
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			callback = function()
-				require("lint").try_lint(nil, { ignore_errors = true })
+				lint.try_lint(nil, { ignore_errors = true })
 			end,
 		})
 	end,
--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -25,11 +25,12 @@ return {
 		})
 
 		-- listen lsp-progress event and refresh lualine immediatly
+		local lualine = require("lualine")
 		vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
 		vim.api.nvim_create_autocmd("User", {
 			group = "lualine_augroup",
 			pattern = "LspProgressStatusUpdated",
-			callback = require("lualine").refresh,
+			callback = lualine.refresh,
 		})
 	end,
 }
