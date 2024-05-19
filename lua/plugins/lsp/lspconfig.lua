5e228827d7bef637bac8c68fcc408562f660b6b6
Daniele Muffato <danixgithub1@gmail.com>
2024-05-19 21:34:22 +0200

    allow removal of cmp engine without crashes

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -32,8 +32,12 @@ return {
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+		local client_capabilities = vim.lsp.protocol.make_client_capabilities()
+		local status, server_capabilities = pcall(require, "cmp_nvim_lsp") -- allow removal of completion engine without crashing
+		local capabilities = {}
+		if status then
+			capabilities = vim.tbl_deep_extend("force", client_capabilities, server_capabilities.default_capabilities())
+		end
 
 		--  Add any additional override configuration in the following tables
 		local servers = {
