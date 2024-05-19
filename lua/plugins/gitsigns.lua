bcfb80376dbf13637b028eb8dd6b822d50acd5b5
Daniele Muffato <danixgithub1@gmail.com>
2024-05-19 23:07:58 +0200

    added events to load treesitter and gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	event = "BufReadPre",
+	event = { "BufReadPre", "BufNewFile", "BufWritePost" },
 	opts = {
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
