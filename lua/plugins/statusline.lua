987f4797c58b104a4275439467f3e393b06c876e
Daniele Muffato <danixgithub1@gmail.com>
2024-05-19 12:41:42 +0200

    removed statusline lsp progress, and replaced with a symbol for when an lsp is active in current buffer

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,16 +1,5 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	dependencies = {
-		"linrongbin16/lsp-progress.nvim",
-		opts = {
-			client_format = function(client_name, spinner, series_messages)
-				local len = #series_messages
-				return len > 0
-						and ("[" .. client_name .. "] " .. spinner .. " " .. table.concat(series_messages, ", ", 1, 1))
-					or nil
-			end,
-		},
-	},
 	config = function()
 		require("lualine").setup({
 			sections = {
@@ -18,19 +7,14 @@ return {
 					"filename",
 					function()
 						-- invoke lsp progress here
-						return require("lsp-progress").progress()
+						if #vim.lsp.get_clients({ bufnr = 0 }) > 0 then
+							return " LSP"
+						else
+							return ""
+						end
 					end,
 				},
 			},
-		})
-
-		-- listen lsp-progress event and refresh lualine immediatly
-		local lualine = require("lualine")
-		vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-		vim.api.nvim_create_autocmd("User", {
-			group = "lualine_augroup",
-			pattern = "LspProgressStatusUpdated",
-			callback = lualine.refresh,
 		})
 	end,
 }
