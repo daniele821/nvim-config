731a15240dc29c547e8e3c4824c5eaa3849fb6b4
Daniele Muffato <danixgithub1@gmail.com>
2024-05-19 12:55:42 +0200

    conform no longer runs unconditionally on init

--- lua/plugins/lsp/autoformat.lua
+++ lua/plugins/lsp/autoformat.lua
@@ -2,28 +2,29 @@ return {
 	-- Autoformat
 	"stevearc/conform.nvim",
 	event = "BufReadPre",
-	opts = {
-		notify_on_error = false,
-		format_on_save = function(bufnr)
-			if vim.g.disable_autoformat then
-				return
-			end
-			-- set disabled filetypes
-			local disable_filetypes = {
-				c = true,
-				cpp = true,
-			}
-			return {
-				timeout_ms = 500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
-		end,
-		-- set formatters by filetype
-		formatters_by_ft = {
-			lua = { "stylua" },
-		},
-	},
-	init = function()
+	config = function()
+		require("conform").setup({
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				if vim.g.disable_autoformat then
+					return
+				end
+				-- set disabled filetypes
+				local disable_filetypes = {
+					c = true,
+					cpp = true,
+				}
+				return {
+					timeout_ms = 500,
+					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				}
+			end,
+			-- set formatters by filetype
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+		})
+
 		-- add mapping to format file
 		vim.keymap.set("n", "<leader>f", function()
 			require("conform").format({ async = true, lsp_fallback = true })
