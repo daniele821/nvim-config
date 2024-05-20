43f3e978dd6d4bbb3dfeeaee8f81dedf172df6a6
Daniele Muffato <danixgithub1@gmail.com>
2024-05-20 20:45:48 +0200

    added formatters info on lualine

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -16,6 +16,21 @@ return {
 						end
 						return ""
 					end,
+					function()
+						-- show active formatters in current buffer
+						if not package.loaded["conform"] then
+							return ""
+						end
+						local conform = require("conform")
+						local fmt = conform.list_formatters(0)
+						local _map = function(elem)
+							return elem.name
+						end
+						if #fmt >= 1 then
+							return "󰉿 " .. vim.iter(fmt):map(_map):join(", ")
+						end
+						return ""
+					end,
 				},
 			},
 		})
