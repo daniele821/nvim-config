e478431a0481dcac5b899cbd386ff9b9a627a3ac
Daniele Muffato <danixgithub1@gmail.com>
2024-05-21 19:04:03 +0200

    changed defaults

--- lua/custom/defaults.lua
+++ lua/custom/defaults.lua
@@ -1,6 +1,31 @@
 return {
-	servers = {},
-	starterpack_lsp = {},
-	formatters_by_ft = {},
+	servers = {
+		lua_ls = {
+			settings = {
+				Lua = {
+					completion = {
+						callSnippet = "Replace",
+					},
+					hint = {
+						enable = true,
+					},
+				},
+			},
+		},
+	},
+	starterpack_lsp = {
+		"stylua",
+		"lua-language-server",
+		"shellcheck",
+		"bash-language-server",
+		"shfmt",
+		"clangd",
+		"python-lsp-server",
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		sh = { "shfmt" },
+		bash = { "shfmt" },
+	},
 	disable_autoformat = true,
 }
