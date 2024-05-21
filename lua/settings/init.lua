21aeb5cf0de91c960bacb2898f1a384788cc58b6
Daniele Muffato <danixgithub1@gmail.com>
2024-05-21 19:40:40 +0200

    fixed bug

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,6 +1,6 @@
 local defaults = require("settings.defaults")
 local status, user = pcall(require,"settings.user")
-if not status then
+if not status or type(user) ~= "table" then
     return defaults
 end
 return vim.tbl_deep_extend("force", defaults, user)
