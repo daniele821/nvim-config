b09c4b99326a92f933c40cc5245f6732fc779bf8
Daniele Muffato <danixgithub1@gmail.com>
2024-05-21 19:16:58 +0200

    fixed customization issues

---
+++ lua/custom/presets/normal.lua
@@ -0,0 +1,31 @@
+return {
+	servers = {
+		lua_ls = {
+			settings = {
+				Lua = {
+					completion = {
+						callSnippet = "Replace",
+					},
+					hint = {
+						enable = true,
+					},
+				},
+			},
+		},
+	},
+	starterpack_lsp = {
+		"stylua",
+		"lua-language-server",
+		"shellcheck",
+		"bash-language-server",
+		"shfmt",
+		"clangd",
+		"python-lsp-server",
+	},
+	formatters_by_ft = {
+		lua = { "stylua" },
+		sh = { "shfmt" },
+		bash = { "shfmt" },
+	},
+	disable_autoformat = true,
+}
