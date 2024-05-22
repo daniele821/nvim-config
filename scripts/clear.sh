7cdd0d7f2f846ea852f69d0d77a4def6f0262a9a
Daniele Muffato <danixgithub1@gmail.com>
2024-05-22 17:07:52 +0200

    small fmt

--- scripts/clear.sh
+++ scripts/clear.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/bash
 
-rm -rf ~/.local/share/nvim/ 
-rm -rf ~/.local/state/nvim/ 
-rm -rf ~/.cache/nvim/ 
-
-
+rm -rf ~/.local/share/nvim/
+rm -rf ~/.local/state/nvim/
+rm -rf ~/.cache/nvim/
--- scripts/list_plugins.sh
+++ scripts/list_plugins.sh
@@ -2,5 +2,4 @@
 
 echo -e "\x1b[31;1mWARNING: this script is one hell of an heuristic, do not rely on it\x1b[m\n" >/dev/stderr
 
-rg / ./lua/plugins/ -I | grep -v require | grep -v -- --  | grep -v lazypath |  while read line; do echo $line; done | sort |  uniq 
-
+rg / ./lua/plugins/ -I | grep -v require | grep -v -- -- | grep -v lazypath | while read line; do echo $line; done | sort | uniq
