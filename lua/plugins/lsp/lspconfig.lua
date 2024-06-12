bf17535d3b6eebdc1d70c536efdf5e59f699ddd7
Daniele Muffato <danixgithub1@gmail.com>
2024-06-12 12:09:22 +0200

    removed neodev

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -8,6 +8,9 @@ local servers = {
 				hint = {
 					enable = true,
 				},
+				diagnostics = {
+					globals = { "vim" },
+				},
 			},
 		},
 	},
@@ -41,12 +44,6 @@ return {
 
 		-- gaps the bridge between completion engine and lsp
 		"hrsh7th/cmp-nvim-lsp",
-
-		-- Useful status updates for LSP.
-		-- { "j-hui/fidget.nvim", opts = {} },
-
-		-- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
 		-- LSP servers and clients are able to communicate to each other what features they support.
