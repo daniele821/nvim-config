3e1181444364668d6751cd53f610604a7e6b0cf0
Daniele Muffato <danixgithub1@gmail.com>
2024-07-02 13:06:12 +0200

    added <a-c> telescope git_status keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,23 @@ return {
 	branch = "0.1.x",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
-		{ "<A-f>", ":Telescope find_files<CR>" },
-		{ "<A-b>", ":Telescope buffers<CR>" },
+		{
+			"<A-f>",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+		},
+		{
+			"<A-b>",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+		},
+		{
+			"<A-c>",
+			function()
+				require("telescope.builtin").git_status()
+			end,
+		},
 	},
 }
