ea52453a5536ba61c1241a7820115c83d1adb1c6
Daniele Muffato <danixgithub1@gmail.com>
2024-07-05 21:38:14 +0200

    config -> opts = {}

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,52 +1,50 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			sections = {
-				lualine_c = {
-					"filename",
-					function()
-						-- invoke lsp progress here
-						local lsp = vim.lsp.get_clients({ bufnr = 0 })
-						local lsp_msg = ""
-						local fmt_msg = ""
-						if #lsp >= 1 then
-							local icon = " "
-							if vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }) then
-								icon = " "
-							end
-							lsp_msg = icon
-								.. vim.iter(lsp)
-									:map(function(elem)
-										return elem.name
-									end)
-									:join(", ")
-								.. " "
+	opts = {
+		sections = {
+			lualine_c = {
+				"filename",
+				function()
+					-- invoke lsp progress here
+					local lsp = vim.lsp.get_clients({ bufnr = 0 })
+					local lsp_msg = ""
+					local fmt_msg = ""
+					if #lsp >= 1 then
+						local icon = " "
+						if vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }) then
+							icon = " "
 						end
-						if package.loaded["conform"] then
-							local fmt = require("conform").list_formatters(0)
-							if #fmt >= 1 or #lsp >= 1 then
-								local icon = "󰉿 "
-								if not vim.g.disable_autoformat then
-									icon = " "
-								end
-								if #fmt >= 1 then
-									fmt_msg = icon
-										.. vim.iter(fmt)
-											:map(function(elem)
-												return elem.name
-											end)
-											:join(", ")
-										.. " "
-								elseif not vim.g.disable_lspformat[vim.bo[0].filetype] then
-									fmt_msg = icon .. "[LSP] "
-								end
+						lsp_msg = icon
+							.. vim.iter(lsp)
+								:map(function(elem)
+									return elem.name
+								end)
+								:join(", ")
+							.. " "
+					end
+					if package.loaded["conform"] then
+						local fmt = require("conform").list_formatters(0)
+						if #fmt >= 1 or #lsp >= 1 then
+							local icon = "󰉿 "
+							if not vim.g.disable_autoformat then
+								icon = " "
+							end
+							if #fmt >= 1 then
+								fmt_msg = icon
+									.. vim.iter(fmt)
+										:map(function(elem)
+											return elem.name
+										end)
+										:join(", ")
+									.. " "
+							elseif not vim.g.disable_lspformat[vim.bo[0].filetype] then
+								fmt_msg = icon .. "[LSP] "
 							end
 						end
-						return lsp_msg .. fmt_msg
-					end,
-				},
+					end
+					return lsp_msg .. fmt_msg
+				end,
 			},
-		})
-	end,
+		},
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,10 @@ return {
 	build = ":TSUpdate",
 	event = { "BufReadPre", "BufNewFile", "BufWritePost" },
 	cmd = { "TSInstall" },
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {},
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+	opts = {
+		ensure_installed = {},
+		auto_install = true,
+		highlight = { enable = true },
+		indent = { enable = true },
+	},
 }
