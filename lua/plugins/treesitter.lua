03ea31f89b0aba51b7be6b83e45a5ae25c8eba40
Daniele Muffato <danixgithub1@gmail.com>
2024-07-05 23:16:36 +0200

    fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,10 +3,12 @@ return {
 	build = ":TSUpdate",
 	event = { "BufReadPre", "BufNewFile", "BufWritePost" },
 	cmd = { "TSInstall" },
-	opts = {
-		ensure_installed = {},
-		auto_install = true,
-		highlight = { enable = true },
-		indent = { enable = true },
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "lua", "markdown", "markdown_inline", "vim", "vimdoc" },
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
