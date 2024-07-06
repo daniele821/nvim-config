cfdfddecf6259b156ea9488a5e2192c1983a169d
Daniele Muffato <danixgithub1@gmail.com>
2024-07-06 13:26:08 +0200

    changed bind to be like 0.11 default new one

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -82,7 +82,7 @@ return {
 				local opts = { buffer = event.buf }
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "grn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "gca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "gra", vim.lsp.buf.code_action, opts)
 			end,
 		})
 
