5db32568c99933f4e9631783dd43764d71bb54e3
Daniele Muffato <danixgithub1@gmail.com>
2024-07-27 19:19:55 +0200

    added mapping to (almost) actually clear terminal buffer

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -1,1 +1,10 @@
+-- exit terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- hacky way to (almost) clear neovim terminal buffer
+vim.keymap.set("t", "<C-l>", function()
+	vim.fn.feedkeys("", "n")
+	local sb = vim.bo.scrollback
+	vim.bo.scrollback = 1
+	vim.bo.scrollback = sb
+end)
