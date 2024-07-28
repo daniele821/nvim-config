4a2d8743f3a812459346d2f9d14466a1e5f13660
Daniele Muffato <danixgithub1@gmail.com>
2024-07-28 18:18:06 +0200

    make formatter timeout not noticeable (if fails, just fmt manually with gff)

--- lua/plugins/lsp/formatter.lua
+++ lua/plugins/lsp/formatter.lua
@@ -21,7 +21,7 @@ return {
 					return
 				end
 				return {
-					timeout_ms = 500,
+					timeout_ms = 50,
 					lsp_fallback = not vim.g.disable_lspformat[vim.bo[bufnr].filetype],
 				}
 			end,
