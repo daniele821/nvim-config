5e3ed8eaa5d23366f733b957c00574b0c97c1cbc
Daniele Muffato <danixgithub1@gmail.com>
2024-08-06 00:41:32 +0200

    added git graph (experimental)

--- init.lua
+++ init.lua
@@ -29,6 +29,36 @@ require("lazy").setup({
 	require("plugins.lsp.lspnvim"),
 	require("plugins.lsp.completions"),
 	require("plugins.lsp.formatter"),
+	{
+		"isakbm/gitgraph.nvim",
+		opts = {
+			symbols = {
+				merge_commit = "M",
+				commit = "*",
+			},
+			format = {
+				timestamp = "%H:%M:%S %d-%m-%Y",
+				fields = { "hash", "timestamp", "author", "branch_name", "tag" },
+			},
+			hooks = {
+				on_select_commit = function(commit)
+					print("selected commit:", commit.hash)
+				end,
+				on_select_range_commit = function(from, to)
+					print("selected range:", from.hash, to.hash)
+				end,
+			},
+		},
+		keys = {
+			{
+				"ggl",
+				function()
+					require("gitgraph").draw({}, { all = true, max_count = 5000 })
+				end,
+				desc = "GitGraph - Draw",
+			},
+		},
+	},
 }, {
 	performance = {
 		rtp = {
