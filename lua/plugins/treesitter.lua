3cf86732c1e221f8e6f7818bf451dc9ef0789b9e
Daniele Muffato <danixgithub1@gmail.com>
2024-10-17 14:22:13 +0200

    added more treesitter ensure installed langs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,18 @@ return {
 	cmd = { "TSInstall", "TSUninstall" },
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "lua", "markdown", "markdown_inline", "vim", "vimdoc", "query", "c", "bash" },
+			ensure_installed = {
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"vim",
+				"vimdoc",
+				"query",
+				"c",
+				"bash",
+				"go",
+				"python",
+			},
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
