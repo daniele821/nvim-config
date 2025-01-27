aa53996a3a33806655c3926311f51bf5914385b5
Daniele Muffato <danixgithub1@gmail.com>
2025-01-27 02:14:56 +0100

    added scrolloff of 5

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -11,26 +11,13 @@ vim.opt.expandtab = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- do not show current mode in status line
+-- various
 vim.opt.showmode = false
-
--- ignore case in searches
 vim.opt.ignorecase = true
-
--- do not highlight search results
 vim.opt.hlsearch = false
-
--- keep signcolumn on by default
 vim.opt.signcolumn = "yes"
-
--- highlight cursor current line
 vim.opt.cursorline = true
-
--- set maximum height for complete menu
 vim.opt.pumheight = 15
-
--- disable right click popup
 vim.opt.mousemodel = "extend"
-
--- disable swap files
 vim.opt.swapfile = false
+vim.opt.scrolloff = 5
