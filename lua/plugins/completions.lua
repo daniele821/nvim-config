cad99f7e332d6537fba4bd8e04d46863f2fdddbf
Daniele Muffato <danixgithub1@gmail.com>
2025-05-12 00:35:33 +0200

    improved blink more

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -6,7 +6,8 @@ return {
         keymap = {
             preset = "none",
             ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
-            ['<C-e>'] = { 'hide' },
+            ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
+            ['<C-e>'] = { 'cancel' },
             ['<C-y>'] = { 'select_and_accept' },
             ['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
             ['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
@@ -14,7 +15,6 @@ return {
             ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
             ['<C-l>'] = { 'snippet_forward', 'fallback' },
             ['<C-h>'] = { 'snippet_backward', 'fallback' },
-            ['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
         },
         appearance = {
             nerd_font_variant = 'mono'
