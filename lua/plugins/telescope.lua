d58d5d4924e30cfb1c0af2b28bd9749c1c6c53f1
Daniele Muffato <danixgithub1@gmail.com>
2025-05-11 07:03:19 +0200

    changes to telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,69 +1,70 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	cmd = "Telescope",
-	branch = "0.1.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-	},
-	keys = {
-		{
-			"<A-f>",
-			function()
-				require("telescope.builtin").find_files()
-			end,
-			mode = { "n", "i" },
-		},
-		{
-			"<A-s-f>",
-			function()
-				require("telescope.builtin").find_files({
-					file_ignore_patterns = { "%.git/" },
-					hidden = true,
-					no_ignore = true,
-					no_ignore_parent = true,
-				})
-			end,
-			mode = { "n", "i" },
-		},
-		{
-			"<A-b>",
-			function()
-				require("telescope.builtin").buffers()
-			end,
-			mode = { "n", "i" },
-		},
-		{
-			"<A-s-g>",
-			function()
-				require("telescope.builtin").grep_string()
-			end,
-			mode = { "n", "i" },
-		},
-		{
-			"<A-g>",
-			function()
-				require("telescope.builtin").live_grep()
-			end,
-			mode = { "n", "i" },
-		},
-	},
-	config = function()
-		local actions = require("telescope.actions")
-		require("telescope").setup({
-			defaults = {
-				mappings = {
-					i = {
-						["<esc>"] = actions.close,
-						["<C-c>"] = actions.close,
-					},
-					n = {
-						["<esc>"] = actions.close,
-						["<C-c>"] = actions.close,
-					},
-				},
-			},
-		})
-	end,
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+        {
+            "<A-f>",
+            function() require("telescope.builtin").find_files() end,
+        },
+        {
+            "<A-s-f>",
+            function()
+                require("telescope.builtin").find_files({
+                    file_ignore_patterns = { "%.git/" },
+                    hidden = true,
+                    no_ignore = true,
+                    no_ignore_parent = true,
+                })
+            end,
+        },
+        {
+            "<A-b>",
+            function() require("telescope.builtin").buffers() end,
+        },
+        {
+            "<A-s-g>",
+            function() require("telescope.builtin").grep_string() end,
+        },
+        {
+            "<A-g>",
+            function() require("telescope.builtin").live_grep() end,
+        },
+    },
+    config = function()
+        local actions = require("telescope.actions")
+        require("telescope").setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<esc>"] = actions.close,
+                        ["<C-c>"] = actions.close,
+                        ["<A-f>"] = function() require("telescope.builtin").find_files() end,
+                        ["<A-s-f>"] = 
+                        function()
+                            require("telescope.builtin").find_files({
+                                file_ignore_patterns = { "%.git/" },
+                                hidden = true,
+                                no_ignore = true,
+                                no_ignore_parent = true,
+                            })
+                        end,
+
+                        ["<A-b>"] = function() require("telescope.builtin").find_files() end,
+                        ["<A-s-g>"] = function() require("telescope.builtin").grep_string() end,
+                        ["<A-g>"] = function() require("telescope.builtin").live_grep() end,
+                    },
+                    n = {
+                        ["<esc>"] = actions.close,
+                        ["<C-c>"] = actions.close,
+                    },
+                },
+            },
+        })
+    end,
 }
 
