ca732ede3ce05987cfa00cdb284ff8518d97fdb3
Daniele Muffato <danixgithub1@gmail.com>
2025-05-16 00:59:39 +0200

    disable mason setting env.PATH, because i am already handling that myself!!!

--- lua/thebest/plugins/mason.lua
+++ lua/thebest/plugins/mason.lua
@@ -1,8 +1,13 @@
 -- make installed binaries ALWAYS available
-vim.env.PATH = vim.fs.joinpath(vim.fn.stdpath("data"), "mason", "bin") .. ":" .. vim.env.PATH
+local install_dir = vim.fs.joinpath(vim.fn.stdpath("data"), "mason")
+local install_bin = vim.fs.joinpath(install_dir, "bin")
+vim.env.PATH =  install_bin .. ":" .. vim.env.PATH
 
 return {
     "mason-org/mason.nvim",
     cmd = {"Mason", "MasonInstall", "MasonUninstall", "MasonUninstallAll"},
-    opts = {},
+    opts = {
+        install_root_dir = install_dir,
+        PATH = "skip",
+    },
 }
