51e36c9d3eaed500a55e567c44f454364824bf54
Daniele Muffato <danixgithub1@gmail.com>
2025-05-16 22:15:09 +0200

    reorg: lsp subdirectory in plugins

--- init.lua
+++ init.lua
@@ -26,9 +26,9 @@ require("lazy").setup({
     require('thebest.plugins.explorer'),
     require('thebest.plugins.gitsigns'),
     require('thebest.plugins.telescope'),
-    require('thebest.plugins.treesitter'),
-    require('thebest.plugins.completions'),
-    require('thebest.plugins.mason'),
+    require('thebest.plugins.lsp.treesitter'),
+    require('thebest.plugins.lsp.completions'),
+    require('thebest.plugins.lsp.mason'),
 }, {
     performance = {
         rtp = {
