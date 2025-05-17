3a7d4fa9b34012b32b11b38c7cfef79b3bfc8208
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 03:41:29 +0200

    small change in refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -27,7 +27,9 @@ function refresh_nvim() {
     }, sync_install = true }" "+qa"
 }
 
-if [[ -v "INLINE_OUTPUT" ]]; then
+if [[ -v "FULL_OUTPUT" ]]; then
+    refresh_nvim 
+else
     tput rmam
     refresh_nvim 2>&1 | while read -r line; do
         if [[ -n "$line" ]]; then
@@ -37,6 +39,4 @@ if [[ -v "INLINE_OUTPUT" ]]; then
     done
     echo -ne "\r\033[2K"
     tput smam
-else
-    refresh_nvim
 fi
