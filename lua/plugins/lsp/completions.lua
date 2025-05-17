75b99397dbd8610554468b6edb7afa9362d3c47f
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 21:41:42 +0200

    do not build luasnip regex completions

--- lua/plugins/lsp/completions.lua
+++ lua/plugins/lsp/completions.lua
@@ -5,10 +5,6 @@ return {
         {
             'L3MON4D3/LuaSnip',
             version = 'v2.*',
-            build = "make install_jsregexp",
-            cond = function()
-                return vim.fn.executable('make') == 1
-            end,
             dependencies = {
                 'rafamadriz/friendly-snippets',
                 config = function()
