d21ed447ead8ff819886107b634fc320024f13a6
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 03:26:48 +0200

    updated list of lang parsers to automagically download

--- refresh.sh
+++ refresh.sh
@@ -4,5 +4,23 @@ rm -rf ~/{.local/{share,state},.cache}/nvim
 
 nvim --headless "+StarterPack" \
     "+lua require('nvim-treesitter.configs').setup { ensure_installed = {       \
-    'bash','rust','cpp','javascript','css','html','python'                      \
+    'bash',             \
+    'cpp',              \
+    'css',              \
+    'dockerfile',       \
+    'git_config',       \
+    'git_rebase',       \
+    'gitattributes',    \
+    'gitcommit',        \
+    'gitignore',        \
+    'go',               \
+    'html',             \
+    'javascript',       \
+    'php',              \
+    'python',           \
+    'rust',             \
+    'sql',              \
+    'toml',             \
+    'typescript',       \
+    'yaml',             \
     }, sync_install = true }" "+qa"
