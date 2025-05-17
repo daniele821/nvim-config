99db170af1bb7f12befde87b0f354a3166a95253
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 02:31:32 +0200

    silence :noh<CR> keymap

--- lua/thebest/configs/keymaps.lua
+++ lua/thebest/configs/keymaps.lua
@@ -1,5 +1,5 @@
--- easily quit terminal 
+-- easily quit terminal
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- easily clear search
-vim.keymap.set("n", "<Esc>", ":noh<CR>")
+vim.keymap.set("n", "<Esc>", ":noh<CR>", { silent = true })
