5d0323157d1bc45d4d0ec306e27966f442af3948
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 15:53:31 +0200

    fmt

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 -- core configurations
-require('thebest.configs.options')
-require('thebest.configs.autocmd')
-require('thebest.configs.keymaps')
+require("thebest.configs.options")
+require("thebest.configs.autocmd")
+require("thebest.configs.keymaps")
 
 -- install lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
+if not vim.uv.fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
     local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
     if vim.v.shell_error ~= 0 then
@@ -22,16 +22,16 @@ vim.opt.rtp:prepend(lazypath)
 
 -- install plugins using lazy
 require("lazy").setup({
-    require('lua.thebest.plugins.core.colorscheme'),
-    require('lua.thebest.plugins.core.explorer'),
-    require('lua.thebest.plugins.core.gitsigns'),
-    require('lua.thebest.plugins.core.telescope'),
-    require('lua.thebest.plugins.core.treesitter'),
-    require('lua.thebest.plugins.lsp.completions'),
-    require('lua.thebest.plugins.lsp.linters'),
-    require('lua.thebest.plugins.lsp.lspconfig'),
-    require('lua.thebest.plugins.lsp.mason'),
-    require('lua.thebest.plugins.lsp.statusline'),
+    require("lua.thebest.plugins.core.colorscheme"),
+    require("lua.thebest.plugins.core.explorer"),
+    require("lua.thebest.plugins.core.gitsigns"),
+    require("lua.thebest.plugins.core.telescope"),
+    require("lua.thebest.plugins.core.treesitter"),
+    require("lua.thebest.plugins.lsp.completions"),
+    require("lua.thebest.plugins.lsp.linters"),
+    require("lua.thebest.plugins.lsp.lspconfig"),
+    require("lua.thebest.plugins.lsp.mason"),
+    require("lua.thebest.plugins.lsp.statusline"),
 }, {
     performance = {
         rtp = {
