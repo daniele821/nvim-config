01506d16b62aa0a0308d7e32ab032dd54bc28f73
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 22:37:52 +0200

    moved code to configure lsp and diagnostics windows and stuff

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -19,3 +19,30 @@ vim.opt.ignorecase = true
 vim.opt.mousemodel = "extend"
 vim.opt.pumheight = 15
 vim.opt.swapfile = false
+
+-- configure diagnostics
+vim.diagnostic.config({
+	severity_sort = true,
+	virtual_text = true,
+	float = { border = "single" },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = "󰅚 ",
+			[vim.diagnostic.severity.WARN] = "󰀪 ",
+			[vim.diagnostic.severity.INFO] = "󰋽 ",
+			[vim.diagnostic.severity.HINT] = "󰌶 ",
+		},
+	},
+})
+
+-- configure lsp floating windows
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+---@diagnostic disable-next-line: duplicate-set-field
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or "single"
+	opts.max_width = opts.max_width or 100
+	opts.max_height = opts.max_height or 15
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
+
