bc4f5472617f299c8987eb1abc6db2fd4f7361eb
Daniele Muffato <danixgithub1@gmail.com>
2025-05-17 15:52:31 +0200

    fmt

--- init.lua
+++ init.lua
@@ -6,17 +6,17 @@ require('thebest.configs.keymaps')
 -- install lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -28,10 +28,10 @@ require("lazy").setup({
     require('lua.thebest.plugins.core.telescope'),
     require('lua.thebest.plugins.core.treesitter'),
     require('lua.thebest.plugins.lsp.completions'),
-    require('lua.thebest.plugins.lsp.statusline'),
-    require('lua.thebest.plugins.lsp.mason'),
-    require('lua.thebest.plugins.lsp.lspconfig'),
     require('lua.thebest.plugins.lsp.linters'),
+    require('lua.thebest.plugins.lsp.lspconfig'),
+    require('lua.thebest.plugins.lsp.mason'),
+    require('lua.thebest.plugins.lsp.statusline'),
 }, {
     performance = {
         rtp = {
