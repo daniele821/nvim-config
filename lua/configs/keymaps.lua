58ceaa439dd220a08b66a755a168dc260a8e3b04
Daniele Muffato <danixgithub1@gmail.com>
2025-05-19 23:15:27 +0200

    disable painful history buffers

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -3,3 +3,7 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- easily clear search
 vim.keymap.set("n", "<Esc>", ":noh<CR>", { silent = true })
+
+-- disable useless history buffers
+vim.keymap.set('n', 'q:', '<nop>')
+vim.keymap.set('n', 'q/', '<nop>')
