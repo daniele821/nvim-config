b9359d2a0db6940e8e2367b4f1365d57b7575af6
Daniele Muffato <danixgithub1@gmail.com>
2025-05-19 05:16:32 +0200

    code to init only on first enter

--- init.lua
+++ init.lua
@@ -57,4 +57,12 @@ local lazy_buf = require("lazy.view").view
 if lazy_buf then
 	vim.api.nvim_buf_delete(lazy_buf.buf, {})
 end
-vim.cmd("StarterPack")
+local first_run_marker = vim.fs.joinpath(vim.fn.stdpath("data"), ".first_run_done")
+local first_run = false
+if not vim.uv.fs_stat(first_run_marker) then
+	first_run = true
+    io.open(first_run_marker, "w"):write("Hi!"):close()
+end
+if not first_run then
+	vim.cmd("StarterPack")
+end
