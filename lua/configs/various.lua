4c8e8ef90ab77c9062391c5532056fd075791b64
Daniele Muffato <danixgithub1@gmail.com>
2025-05-20 03:54:06 +0200

    removed useless lspconfig plugin file

--- lua/configs/various.lua
+++ lua/configs/various.lua
@@ -65,3 +65,22 @@ vim.api.nvim_create_user_command("InlayHintsToggle", function()
 	end
 	vim.lsp.inlay_hint.enable(not hints)
 end, {})
+
+-- enable lsp servers
+vim.lsp.enable(require("utils.configs").lsp_to_enable)
+
+-- stop lsp on exit
+vim.api.nvim_create_autocmd("VimLeavePre", {
+	callback = function()
+		vim.iter(vim.lsp.get_clients()):each(function(client)
+			client:stop()
+		end)
+	end,
+})
+
+-- additional keymaps for buffers with lsp attached
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(event)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
+	end,
+})
