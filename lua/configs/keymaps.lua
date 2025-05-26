13577c27e44835ce63f43d12894507d43a490122
Daniele Muffato <danixgithub1@gmail.com>
2025-05-26 17:44:08 +0200

    remove tab commands

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -8,7 +8,3 @@ vim.keymap.set("n", "<Esc>", ":noh<CR>", { silent = true })
 vim.keymap.set("n", "q:", "<nop>")
 vim.keymap.set("n", "q/", "<nop>")
 vim.keymap.set("n", "q?", "<nop>")
-
--- add / remove tabs
-vim.keymap.set("n", "<a-t>", ":tabnew<CR>", { silent = true })
-vim.keymap.set("n", "<a-s-t>", ":tabclose<CR>", { silent = true })
