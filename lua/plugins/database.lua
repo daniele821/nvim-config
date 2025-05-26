4513e8cedb6a3e17edf8f85e9fb0d12a881b7293
Daniele Muffato <danixgithub1@gmail.com>
2025-05-26 17:26:07 +0200

    add autocmd for tweaks when operating in dbui tabs

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -22,8 +22,29 @@ return {
 				vim.keymap.set("n", "h", "<Plug>(DBUI_ToggleDetails)", { buffer = true })
 			end,
 		})
-        -- for query file operations, run the following, whilst in the query buffer:
-        -- :execute "normal \<Plug>(DBUI_SaveQuery)"
-        -- :execute "normal \<Plug>(DBUI_EditBindParameters)"
+
+        -- fixes for buffers in the same tab scope as database explorer
+		vim.api.nvim_create_autocmd({ "BufEnter", "WinClosed" }, {
+			callback = function()
+				local tabpage = vim.api.nvim_get_current_tabpage()
+				local wins = vim.api.nvim_tabpage_list_wins(tabpage)
+				local dbui_present = false
+				for _, win in ipairs(wins) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+					if ft == "dbui" then
+						dbui_present = true
+						break
+					end
+				end
+				for _, win in ipairs(wins) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+					if ft == "json" then
+						vim.diagnostic.enable(not dbui_present, { bufnr = buf })
+					end
+				end
+			end,
+		})
 	end,
 }
