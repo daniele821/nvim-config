7bde3a171fa3908a1523ecc628ac2562bfe5b603
Daniele Muffato <danixgithub1@gmail.com>
2025-05-26 08:19:30 +0200

    make mappings buffer only

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -15,11 +15,11 @@ return {
 		vim.api.nvim_create_autocmd("FileType", {
 			pattern = "dbui",
 			callback = function()
-				vim.keymap.set("n", "<CR>", "<Plug>(DBUI_SelectLine)", {})
-				vim.keymap.set("n", "o", "<Plug>(DBUI_SelectLine)", {})
-				vim.keymap.set("n", "s", "<Plug>(DBUI_DeleteLine)", {})
-				vim.keymap.set("n", "a", "<Plug>(DBUI_AddConnection)", {})
-				vim.keymap.set("n", "h", "<Plug>(DBUI_ToggleDetails)", {})
+				vim.keymap.set("n", "<CR>", "<Plug>(DBUI_SelectLine)", { buffer = true })
+				vim.keymap.set("n", "o", "<Plug>(DBUI_SelectLine)", { buffer = true })
+				vim.keymap.set("n", "d", "<Plug>(DBUI_DeleteLine)", { buffer = true })
+				vim.keymap.set("n", "a", "<Plug>(DBUI_AddConnection)", { buffer = true })
+				vim.keymap.set("n", "h", "<Plug>(DBUI_ToggleDetails)", { buffer = true })
 			end,
 		})
 	end,
