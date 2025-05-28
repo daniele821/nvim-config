06c3fe950f5a442627bbd998aaedf3b90b9a9d23
Daniele Muffato <danixgithub1@gmail.com>
2025-05-28 15:25:29 +0200

    removed q: q/ q? remaps, as those interfere with macro recording

--- lua/configs/keymaps.lua
+++ lua/configs/keymaps.lua
@@ -3,8 +3,3 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 
 -- easily clear search
 vim.keymap.set("n", "<Esc>", ":noh<CR>", { silent = true })
-
--- disable useless history buffers
-vim.keymap.set("n", "q:", "<nop>")
-vim.keymap.set("n", "q/", "<nop>")
-vim.keymap.set("n", "q?", "<nop>")
