28f82f8c636d35b83737d9947b49798e80831717
Daniele Muffato <danixgithub1@gmail.com>
2025-05-31 14:07:37 +0200

    fixed lua lsp to use neovim libs only when inside the config files

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,21 +1,34 @@
 -- DOCUMENTATION: https://luals.github.io/wiki/settings/
-local function is_nvim_project()
-	local config_dir = vim.fn.resolve(vim.fn.stdpath("config"))
-	---@diagnostic disable-next-line: undefined-field
-	local cwd = vim.fn.resolve(vim.uv.cwd())
-	return cwd:sub(1, #config_dir) == config_dir
-end
-
 return {
+	on_init = function(client)
+		if client.workspace_folders then
+			local path = vim.fn.resolve(client.workspace_folders[1].name)
+			local conf = vim.fn.resolve(vim.fn.stdpath("config"))
+			if path == conf then
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						version = "LuaJIT",
+						path = {
+							"lua/?.lua",
+							"lua/?/init.lua",
+						},
+					},
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- "${3rd}/luv/library",
+							-- "${3rd}/busted/library",
+						},
+					},
+				})
+			end
+		end
+	end,
 	settings = {
 		Lua = {
 			completion = {
 				autoRequire = false,
-			},
-			workspace = {
-				checkThirdParty = false,
-				-- black magic to show vim api in ~/.config/nvim and nowhere else!
-				library = is_nvim_project() and { vim.env.VIMRUNTIME } or {},
 			},
 			hint = {
 				enable = true,
