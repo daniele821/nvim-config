c335e0953b097e54a6845e6f79ff2f76bdd8aa58
Daniele Muffato <danixgithub1@gmail.com>
2025-06-06 20:59:22 +0200

    keymapt to format using lsp

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -5,6 +5,7 @@ vim.lsp.enable(require("utils.configs").lsp_to_enable)
 vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(event)
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = event.buf })
+		vim.keymap.set("n", "grf", vim.lsp.buf.format, { buffer = event.buf })
 	end,
 })
 
