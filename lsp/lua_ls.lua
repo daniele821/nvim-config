d60d83311473baee4b9797c9af6d6c660223271f
Daniele Muffato <danixgithub1@gmail.com>
2025-06-06 16:48:29 +0200

    enable luv for luals in nvim files

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -16,7 +16,7 @@ return {
 						checkThirdParty = false,
 						library = {
 							vim.env.VIMRUNTIME,
-							-- "${3rd}/luv/library",
+							"${3rd}/luv/library",
 							-- "${3rd}/busted/library",
 						},
 					},
