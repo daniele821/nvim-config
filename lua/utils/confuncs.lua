a5c14b65e8526436ee9db3ef43698c8fb58ea3a9
Daniele Muffato <danixgithub1@gmail.com>
2025-06-07 13:54:08 +0200

    added comment

--- lua/utils/confuncs.lua
+++ lua/utils/confuncs.lua
@@ -1,8 +1,10 @@
 local configs = require("utils.configs")
 return {
+    -- array of all filetypes for which to enable treesitter
     all_language_parsers = (function()
         return vim.tbl_keys(configs.parsers)
     end)(),
+    -- array of all treesitter parsers to download
     to_install_parsers = (function()
         local res = {}
         for lang, opts in pairs(configs.parsers) do
@@ -12,6 +14,7 @@ return {
         end
         return res
     end)(),
+    -- table of languages to remap to other languages
     to_remap_parsers = (function()
         local res = {}
         for lang, opts in pairs(configs.parsers) do
