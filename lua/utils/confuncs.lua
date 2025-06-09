5192d78448b6b578561db9f8415a5ec6573f3e80
Daniele Muffato <danixgithub1@gmail.com>
2025-06-09 22:21:56 +0200

    simplify confuncs

--- lua/utils/confuncs.lua
+++ lua/utils/confuncs.lua
@@ -1,45 +1,35 @@
 local configs = require("utils.configs")
 
 return {
-	all_language_parsers = (function()
-		return configs.parsers
-	end)(),
-	to_enable_lsp = (function()
-		return vim.iter(configs.lsps)
-			:filter(function(_, opts)
-				return opts.lsp ~= nil
-			end)
-			:map(function(_, opts)
-				return opts.lsp
-			end)
-			:totable()
-	end)(),
-	to_install_packages = (function()
-		return vim.tbl_keys(configs.lsps)
-	end)(),
-	use_local_packages = (function()
-		return vim.iter(configs.lsps)
-			:filter(function(_, opts)
-				return opts.lcl ~= nil
-			end)
-			:fold({}, function(acc, lsp, opts)
-				acc[lsp] = opts.bin or lsp
-				return acc
-			end)
-	end)(),
-	formatters_by_ft = (function()
-		return vim.iter(configs.lsps)
-			:filter(function(_, opts)
-				return opts.fmt ~= nil
-			end)
-			:fold({}, function(acc, lsp, opts)
-				for _, lang in ipairs(opts.fmt) do
-					if acc[lang] == nil then
-						acc[lang] = {}
-					end
-					table.insert(acc[lang], opts.bin or lsp)
+	all_language_parsers = configs.parsers,
+	to_enable_lsp = vim.iter(configs.lsps)
+		:filter(function(_, opts)
+			return opts.lsp ~= nil
+		end)
+		:map(function(_, opts)
+			return opts.lsp
+		end)
+		:totable(),
+	to_install_packages = vim.tbl_keys(configs.lsps),
+	use_local_packages = vim.iter(configs.lsps)
+		:filter(function(_, opts)
+			return opts.lcl ~= nil
+		end)
+		:fold({}, function(acc, lsp, opts)
+			acc[lsp] = opts.bin or lsp
+			return acc
+		end),
+	formatters_by_ft = vim.iter(configs.lsps)
+		:filter(function(_, opts)
+			return opts.fmt ~= nil
+		end)
+		:fold({}, function(acc, lsp, opts)
+			for _, lang in ipairs(opts.fmt) do
+				if acc[lang] == nil then
+					acc[lang] = {}
 				end
-				return acc
-			end)
-	end)(),
+				table.insert(acc[lang], opts.bin or lsp)
+			end
+			return acc
+		end),
 }
