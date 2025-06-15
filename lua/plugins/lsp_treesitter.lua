2d1b80bdbeb4735c1746da2a74ffa3788951c169
Daniele Muffato <danixgithub1@gmail.com>
2025-06-15 21:09:37 +0200

    lazy load treesitter

--- lua/plugins/lsp_treesitter.lua
+++ lua/plugins/lsp_treesitter.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-treesitter/nvim-treesitter",
 	branch = "main",
 	build = ":TSUpdate",
+	event = "VeryLazy",
 	config = function()
 		vim.api.nvim_create_autocmd("Filetype", {
 			callback = function()
@@ -10,5 +11,8 @@ return {
 				end)
 			end,
 		})
+		vim.schedule(function()
+			pcall(vim.treesitter.start)
+		end)
 	end,
 }
