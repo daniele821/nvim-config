7b47faf9068f69e13e8cf4dfb3ae9c6c9e041ca0
Daniele Muffato <danixgithub1@gmail.com>
2025-07-05 13:26:20 +0200

    fix indentation

--- lua/plugins/lsp_treesitter.lua
+++ lua/plugins/lsp_treesitter.lua
@@ -4,15 +4,18 @@ return {
 	build = ":TSUpdate",
 	event = "VeryLazy",
 	config = function()
+		local function parse()
+			vim.schedule(function()
+				local success, _ = pcall(vim.treesitter.start)
+				if success then
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+                    vim.bo.indentexpr = ""
+				end
+			end)
+		end
 		vim.api.nvim_create_autocmd("Filetype", {
-			callback = function()
-				vim.schedule(function()
-					pcall(vim.treesitter.start)
-				end)
-			end,
+			callback = parse,
 		})
-		vim.schedule(function()
-			pcall(vim.treesitter.start)
-		end)
+		parse()
 	end,
 }
