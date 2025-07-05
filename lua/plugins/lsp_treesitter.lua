6ddb925b37fa51a1796e142b6764b3d7982831c3
Daniele Muffato <danixgithub1@gmail.com>
2025-07-05 13:38:59 +0200

    fix indent/folding

--- lua/plugins/lsp_treesitter.lua
+++ lua/plugins/lsp_treesitter.lua
@@ -6,11 +6,9 @@ return {
 	config = function()
 		local function parse()
 			vim.schedule(function()
-				local success, _ = pcall(vim.treesitter.start)
-				if success then
-					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                    vim.bo.indentexpr = ""
-				end
+				pcall(vim.treesitter.start)
+				vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.bo.indentexpr = ""
 			end)
 		end
 		vim.api.nvim_create_autocmd("Filetype", {
