ba80184bdf5463b2972173cbe91d53a44498fb07
Daniele Muffato <danixgithub1@gmail.com>
2025-07-06 16:59:09 +0200

    very simplified neovim, no deps, no complexity involved

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -30,25 +30,3 @@ function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
 	opts.max_height = opts.max_height or 15
 	return orig_util_open_floating_preview(contents, syntax, opts, ...)
 end
-
--- auto command to install parsers and lsps
-vim.api.nvim_create_user_command("StarterPack", function()
-	-- download parsers
-	local res = require("nvim-treesitter").install(require("utils.configs").parsers)
-	if #vim.api.nvim_list_uis() == 0 then
-		res:wait(300000)
-	end
-	-- download lsps
-	local mason_registry = require("mason-registry")
-	local configs = require("utils.configs")
-	local to_install_lsps = configs.lsps
-	local installed_lsps = mason_registry.get_installed_package_names()
-	local missing_lsps = vim.iter(to_install_lsps)
-		:filter(function(elem)
-			return not vim.tbl_contains(installed_lsps, elem)
-		end)
-		:join(" ")
-	if missing_lsps ~= "" then
-		vim.cmd("MasonInstall " .. missing_lsps)
-	end
-end, {})
--- plugin/statusline.lua
+++ plugin/statusline.lua
@@ -10,10 +10,6 @@ function M.setup()
 		"%<%{%v:lua._G.Statusline.filename()%}",
 		"%m%r%y",
 		"%=",
-		"%<%{v:lua._G.Statusline.lspList()}",
-		"%<%{v:lua._G.Statusline.formatterList()}",
-		"%<%{v:lua._G.Statusline.linterList()}",
-		"%=",
 		"%{&ff}",
 		"%l:%v",
 		"%P",
@@ -75,53 +71,6 @@ function M.progressHandler(_, result, ctx)
 	if old_msg ~= new_msg then
 		vim.cmd("redrawstatus")
 	end
-end
-
-function M.lspList()
-	local lsps = vim.lsp.get_clients({ bufnr = 0 })
-	if #lsps > 0 then
-		local lsp_icon = " "
-		local lsp_names = vim.iter(lsps)
-			:map(function(elem)
-				return elem.name
-			end)
-			:map(function(lsp)
-				local tmp = M._lspProgress[lsp] or {}
-				local msg = tmp.message or ""
-				return lsp .. msg
-			end)
-			:join(", ")
-		return lsp_icon .. lsp_names
-	end
-	return ""
-end
-
-function M.formatterList()
-	local formatters = vim.iter(require("utils.configs").formatters_by_ft[vim.bo.filetype] or {})
-		:filter(function(linter)
-			return vim.fn.executable(linter) == 1
-		end)
-		:totable()
-	if #formatters > 0 then
-		local formatter_icon = " "
-		local formatter_names = vim.iter(formatters):join(", ")
-		return formatter_icon .. formatter_names
-	end
-	return ""
-end
-
-function M.linterList()
-	local linters = vim.iter(require("utils.configs").linters_by_ft[vim.bo.filetype] or {})
-		:filter(function(linter)
-			return vim.fn.executable(linter) == 1
-		end)
-		:totable()
-	if #linters > 0 then
-		local lint_icon = " "
-		local linter_names = vim.iter(linters):join(", ")
-		return lint_icon .. linter_names
-	end
-	return ""
 end
 
 function M.filename()
