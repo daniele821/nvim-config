5d1272728331dc86daa2936c7beb5d47b6489b02
Daniele Muffato <danixgithub1@gmail.com>
2025-07-06 23:06:39 +0200

    simplify statusline code

--- plugin/statusline.lua
+++ plugin/statusline.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-M._lspProgress = {}
-
 _G.Statusline = M
 
 function M.setup()
@@ -14,63 +12,6 @@ function M.setup()
 		"%l:%v",
 		"%P",
 	}):join(" ")
-
-	-- intercept progress messages
-	vim.lsp.handlers["$/progress"] = M.progressHandler
-
-	-- autocmd to refresh statusline when necessary
-	vim.api.nvim_create_autocmd("LspAttach", {
-		callback = function()
-			vim.cmd("redrawstatus")
-		end,
-	})
-	vim.api.nvim_create_autocmd("LspDetach", {
-		callback = function()
-			vim.defer_fn(function()
-				vim.cmd("redrawstatus")
-			end, 500)
-		end,
-	})
-end
-
-function M.progressHandler(_, result, ctx)
-	local client = vim.lsp.get_client_by_id(ctx.client_id)
-	local token = result.token
-	local value = result.value
-	if not client or not token or not value then
-		return
-	end
-	local is_end_kind = value.kind == "end"
-	local percentage = value.percentage
-
-	local old_token = vim.tbl_get(M._lspProgress, client.name, "token")
-	local old_msg = vim.tbl_get(M._lspProgress, client.name, "message")
-	local new_token = old_token
-	local new_msg = old_msg
-	if not old_token then
-		if percentage then
-			new_token = token
-			new_msg = " (" .. percentage .. "%)"
-		end
-	else
-		if old_token == token then
-			if is_end_kind then
-				new_token = nil
-				new_msg = ""
-			elseif percentage then
-				new_msg = " (" .. percentage .. "%)"
-			end
-		end
-	end
-
-	M._lspProgress[client.name] = {
-		token = new_token,
-		message = new_msg,
-	}
-
-	if old_msg ~= new_msg then
-		vim.cmd("redrawstatus")
-	end
 end
 
 function M.filename()
